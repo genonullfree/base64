588423b1b686b78e61a6702469ebfcc1998b8cbd
geno nullfree nullfree.geno@gmail.com <nullfree.geno@gmail.com>
2020-07-18 16:13:21 -0400

    adding base64 decode

--- src/base64.rs
+++ src/base64.rs
@@ -1,3 +1,10 @@
+const POSSIBLE: [char; 64] = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+    'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1',
+    '2', '3', '4', '5', '6', '7', '8', '9', '+', '/',
+];
+
 pub fn encode(bytes: &mut Vec<u8>) -> Vec<char> {
     let mut intermediate: Vec<u8> = Vec::new();
 
@@ -44,6 +51,59 @@ pub fn encode(bytes: &mut Vec<u8>) -> Vec<char> {
     output
 }
 
+pub fn decode(bytes: &mut Vec<char>) -> Vec<u8> {
+    let mut output: Vec<u8> = Vec::new();
+    let mut padding = 0;
+
+    // if the lengh is not a multiple of 4, this is an error
+    if (bytes.len() % 4) != 0 {
+        // return an empty vec
+        return output;
+    }
+
+    if bytes.last() == Some(&'=') {
+        bytes.pop();
+        padding += 1;
+        if bytes.last() == Some(&'=') {
+            bytes.pop();
+            padding +=1;
+        }
+    }
+
+    let mut translated: Vec<u8> = translate_to_u6(bytes);
+
+    for _ in 0..padding {
+        translated.push(0);
+    }
+
+    output = u6_to_u8(translated);
+
+    for _ in 0..padding {
+        output.pop();
+    }
+
+    output
+}
+
+fn u6_to_u8(input: Vec<u8>) -> Vec<u8> {
+    let mut output: Vec<u8> = Vec::new();
+
+    for i in (0..input.len()).step_by(4) {
+        output.push(input[i] << 2 | input[i+1] >> 4);
+        output.push(input[i+1] << 4 | input[i+2] >> 4);
+        output.push(input[i+2] << 6 | input[3]);
+    }
+
+    // return Vec<u8>
+    output
+}
+
+fn translate_to_u6(input: &mut Vec<char>) -> Vec<u8> {
+    let u6vec: Vec<u8> = input.into_iter().map(|x| POSSIBLE.iter().position(|y| x == y).unwrap() as u8).collect();
+
+    u6vec
+}
+
 fn munch_bytes(group: &[u8]) -> Vec<u8> {
     let mut out: Vec<u8> = Vec::new();
 
@@ -58,15 +118,9 @@ fn munch_bytes(group: &[u8]) -> Vec<u8> {
 }
 
 fn match_to_b64_char(input: &Vec<u8>) -> Vec<char> {
-    let possible = [
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-        'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-        'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1',
-        '2', '3', '4', '5', '6', '7', '8', '9', '+', '/',
-    ];
 
     // map and collect the translations into an output vector
-    let b64: Vec<char> = input.into_iter().map(|x| possible[*x as usize]).collect();
+    let b64: Vec<char> = input.into_iter().map(|x| POSSIBLE[*x as usize]).collect();
 
     // return Vec<char>
     b64
